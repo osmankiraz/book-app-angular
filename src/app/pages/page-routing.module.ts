638fa25e7a64f6313b0ccc95c432fa81186c41d2
Osman Kiraz <osmnkiraz11@gmail.com>
2022-08-21 22:15:24 +0300

    Updated page-routing.module.ts

--- src/app/pages/page-routing.module.ts
+++ src/app/pages/page-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLayoutComponent } from '../layouts/admin-layout/admin-layout.component';
 import { MainLayoutComponent } from '../layouts/main-layout/main-layout.component';
+import { AdminCategoryNeweditComponent } from './admin-category-newedit/admin-category-newedit.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { HomeComponent } from './home/home.component';
 
@@ -18,6 +19,14 @@ const routes: Routes = [
       {
         path: '',
         component: AdminHomeComponent,
+      },
+      {
+        path: 'category',
+        component: AdminCategoryNeweditComponent,
+      },
+      {
+        path: 'category/:id',
+        component: AdminCategoryNeweditComponent,
       },
     ],
   },
