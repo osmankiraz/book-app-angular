fe1905a1eb743889caf3854b7e9be75c06cb4448
Osman Kiraz <osmnkiraz11@gmail.com>
2022-08-24 22:49:12 +0300

    Updated page-routing.module.ts

--- src/app/pages/page-routing.module.ts
+++ src/app/pages/page-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    children: [{ path: '', component: HomeComponent }],
+    children: [{ path: '', component: HomeComponent },{ path: 'kategori/:id', component: HomeComponent }],
   },
   {
     path: 'admin',
