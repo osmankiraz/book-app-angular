26bf0d294699ec65c5145ed6ea76f8a62529c8de
Osman Kiraz <osmnkiraz11@gmail.com>
2022-08-24 22:50:59 +0300

    Updated home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,36 @@
+import { ActivatedRoute } from '@angular/router';
+import { Book } from './../../models/book';
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  books: Book[] = [];
+  categoryId: string | null | undefined;
 
-  constructor() { }
+  constructor(
+    private bookService: BookService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.categoryId = params.get('id');
+      if (this.categoryId) {
+        this.bookService
+          .getBooksByCategoryId(this.categoryId)
+          .subscribe((response) => {
+            this.books = response;
+          });
+      } else {
+        this.bookService.getBooks().subscribe((response) => {
+          this.books = response;
+        });
+      }
+    });
   }
-
 }
